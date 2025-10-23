693cd9ffd8d51389785ab1de78252fa2ae25594e
Alex Jarabi <jarabialex@gmail.com>
2025-10-23 15:35:51 +0300

    Add user phone number to phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,8 +1,11 @@
 import { useState } from 'react';
 
 const App = () => {
-    const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+    const [persons, setPersons] = useState([
+        { name: 'Arto Hellas', phone: '040-1234567' },
+    ]);
     const [newName, setNewName] = useState('');
+    const [newPhone, setNewPhone] = useState('');
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -17,10 +20,14 @@ const App = () => {
             ) {
                 alert(`${cleanedName} is already added to phonebook`);
             } else {
-                setPersons([...persons, { name: cleanedName }]);
+                setPersons([
+                    ...persons,
+                    { name: cleanedName, phone: newPhone },
+                ]);
             }
         }
         setNewName('');
+        setNewPhone('');
     };
 
     return (
@@ -35,12 +42,21 @@ const App = () => {
                     />
                 </div>
                 <div>
+                    number:{' '}
+                    <input
+                        value={newPhone}
+                        onChange={(e) => setNewPhone(e.target.value)}
+                    />
+                </div>
+                <div>
                     <button type='submit'>add</button>
                 </div>
             </form>
             <h2>Numbers</h2>
             {persons.map((person) => (
-                <p key={person.name}>{person.name}</p>
+                <p key={person.name}>
+                    {person.name} {person.phone}
+                </p>
             ))}
         </div>
     );

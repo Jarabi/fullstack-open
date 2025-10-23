43937ee719218ae40458f9cb4ebac886ac8b9262
Alex Jarabi <jarabialex@gmail.com>
2025-10-23 14:53:02 +0300

    Add user to phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,19 +4,32 @@ const App = () => {
     const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
     const [newName, setNewName] = useState('');
 
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        if (newName) setPersons([...persons, { name: newName }]);
+        setNewName('');
+    };
+
     return (
         <div>
             <h2>Phonebook</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div>
-                    name: <input />
+                    name:{' '}
+                    <input
+                        value={newName}
+                        onChange={(e) => setNewName(e.target.value)}
+                    />
                 </div>
                 <div>
                     <button type='submit'>add</button>
                 </div>
             </form>
             <h2>Numbers</h2>
-            ...
+            {persons.map((person) => (
+                <p key={person.name}>{person.name}</p>
+            ))}
         </div>
     );
 };

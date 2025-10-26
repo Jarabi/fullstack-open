beb6e7ab9099b7ad4cc7ab472eb24409fc4c44aa
Alex Jarabi <jarabialex@gmail.com>
2025-10-27 01:45:13 +0300

    Extract language component

--- part2/countries/src/components/CountryDetails.jsx
+++ part2/countries/src/components/CountryDetails.jsx
@@ -1,19 +1,27 @@
-const CountryDetails = ({country}) => {
-    const {name, capital, area, languages, flag} = country
-    
+import Language from './Language';
+
+const CountryDetails = ({ country }) => {
+    const { name, capital, area, languages, flag } = country;
+
     return (
-    <>
-        <h1>{name.common}</h1>
-        <p><strong>Capital</strong> {capital[0]}</p>
-        <p><strong>Area</strong> {area.toLocaleString()}</p>
+        <>
+            <h1>{name.common}</h1>
+            <p>
+                <strong>Capital</strong> {capital[0]}
+            </p>
+            <p>
+                <strong>Area</strong> {area.toLocaleString()}
+            </p>
 
-        <h2>Languages</h2>
-        <ul>
-            {Object.values(languages).map(language => <li key={language}>{language}</li>)}
-        </ul>
-        <span style={{fontSize:'6rem'}}>{flag}</span>
-    </>
-    )
-}
+            <h2>Languages</h2>
+            <ul>
+                {Object.values(languages).map((language) => (
+                    <Language language={language} />
+                ))}
+            </ul>
+            <span style={{ fontSize: '6rem' }}>{flag}</span>
+        </>
+    );
+};
 
-export default CountryDetails
+export default CountryDetails;

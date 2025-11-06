240c90a6f42b367b4fced3ec62f22069b4228b09
Alex Jarabi <jarabialex@gmail.com>
2025-11-07 00:35:27 +0300

    feat: display the information for a single phonebook entry

--- part3/persons/index.js
+++ part3/persons/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const persons = [
     {
         id: '1',
@@ -24,20 +26,29 @@ const persons = [
     },
 ];
 
-app.get('/', (req, res) => res.send('<h1>Persons Diary</h1>'));
-
-app.get('/info', (req, res) => {
-    const message = `<p>Phonebook has info for ${
-        persons.length
-    } people</p><p>${new Date()}</p>`;
-
-    res.send(message);
-});
+app.get('/', (req, res) => res.send('<h1>Persons Diary</p>'));
 
 app.get('/api/persons', (req, res) => {
     res.json(persons);
 });
 
+app.get('/info', (req, res) => {
+    const message = `<p>Phonebook has info for ${
+        persons.length
+    } people</p><p>${new Date()}</p>`;
+    res.send(message);
+});
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = req.params.id;
+    const person = persons.find((person) => person.id === id);
+
+    if (!person) {
+        res.status(404).end();
+    } else {
+        res.json(person);
+    }
+});
 
 const PORT = 3001;
 app.listen(PORT);
